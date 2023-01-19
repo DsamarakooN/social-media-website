9bd986ea30449fce9fef9b332edca31bf52c1962
Dhanushka Samarakoon <99551588+DsamarakooN@users.noreply.github.com>
2023-01-19 15:06:59 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [age, setAge] = useState(0);
+
+  const increasAge = () => {
+    setAge(age + 1);
+  };
+
   return (
     <div className="App">
-      <User name = "Dhanush"  age = {21} emails = "dhanush@gmail.com"/>
-      <User name = "Pulara"  age = {21} emails = "Pulara@gmail.com"/> 
-      <Product category = "Apple" price = "$123"/> 
+      {age} <br/>
+      <button onClick={increasAge}>Increase Age</button>
     </div>
   );
 }
-
-
 
 export default App;
